16f25b334effa61368023211e132cb77f1802df9
TheDummyUser <108084914+TheDummyUser@users.noreply.github.com>
2024-12-17 13:06:35 +0530

    added zed, zsh plugins and few more

--- config/zsh/default.nix
+++ config/zsh/default.nix
@@ -7,13 +7,20 @@
 {
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
     shellAliases = {
       ll = "ls -l";
       update = "cd ~/nixflakes && sudo nixos-rebuild switch --flake .#default &&cd ~/";
     };
     oh-my-zsh = {
       enable = true;
-      # plugins = [ "git" "thefuck" ];
+      plugins = [
+        "git"
+        "thefuck"
+        "eza"
+      ];
       theme = "gentoo";
     };
   };
