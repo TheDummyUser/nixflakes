136602b59afe7ab08c7ed8228427a6ff04ef1591
TheDummyUser <abhiram.reddy122002@gmail.com>
2025-01-17 01:20:13 +0530

    changed to new drive, added dualboot option

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,24 +8,24 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b975f5b2-6820-4fa4-b358-162b6c9129a6";
+    { device = "/dev/disk/by-uuid/028c2e00-838f-479c-8e99-bd48384cc1bb";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/F96A-A256";
+    { device = "/dev/disk/by-uuid/88F4-F161";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/153cbaeb-12f8-4b96-98f5-ea1522b174f2"; }
+    [ { device = "/dev/disk/by-uuid/1e7af835-39d6-47af-83f5-cd9ce28b2482"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -34,7 +34,7 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f0u5.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f0u6.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
