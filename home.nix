0679162b71358baafbd85968bbb0e2b6ad34777e
TheDummyUser <108084914+TheDummyUser@users.noreply.github.com>
2025-09-07 00:07:08 +0530

    fixed emacs a bit, added cpp bin for image change etc

--- home.nix
+++ home.nix
@@ -108,12 +108,82 @@
       identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
     };
   };
-};
+  };
+
+# services.mpd = {
+#   enable = true;
+#   musicDirectory = "/home/gabbar/music";
+#   user = "gabbar";
+ 
+
+#   # Optional:
+#   network.listenAddress = "any"; # if you want to allow non-localhost connections
+#   network.startWhenNeeded = true; # systemd feature: only start MPD service upon connection to its socket
+# };
+  services.mpd = {
+    enable = true;
+    musicDirectory = "/mnt/Localdisk/Media/music";
+    
+    # Use PipeWire for audio output (recommended for modern NixOS)
+    extraConfig = ''
+      audio_output {
+        type "pipewire"
+        name "PipeWire Output"
+      }
+    '';
+    
+    # Optional: Enable socket activation for automatic startup
+    # startWhenNeeded = true;
+  };
+
+# programs.ncmpcpp = {
+#     enable = true;
+#     mpdMusicDir = "/mnt/Localdisk/Media/music";
+    
+#     # Optional: Custom ncmpcpp configuration
+#     settings = {
+#       mpd_host = "localhost";
+#       mpd_port = "6600";
+#       mpd_connection_timeout = "5";
+#       mpd_crossfade_time = "5";
+      
+#       # UI Configuration
+#       user_interface = "alternative";
+#       header_visibility = "yes";
+#       statusbar_visibility = "yes";
+#       titles_visibility = "yes";
+      
+#       # Colors
+#       colors_enabled = "yes";
+#       main_window_color = "white";
+#       # main_window_highlight_color = "blue";
+#       header_window_color = "cyan";
+#       volume_color = "red";
+#       progressbar_color = "cyan";
+      
+#       # Playlist settings
+#       playlist_display_mode = "columns";
+#       autocenter_mode = "yes";
+#       # fancy_scrolling = "yes";
+      
+#       # Format strings
+#       song_list_format = "{$3%n │ $9}{$7%a - $9}{$5%t$9}|{$8%f$9}$R{$6 │ %b$9}{$3 │ %l$9}";
+#       song_status_format = " $2%a $4⟫$3⟫ $8%t $4⟫$3⟫ $5%b ";
+      
+#       # Other settings
+#       display_bitrate = "yes";
+#       ignore_leading_the = "yes";
+#       cyclic_scrolling = "yes";
+#     };
+#   };
 
 
+  
   home.packages = with pkgs; [
     age
     sops
     openssh
+    mpc-cli
+    rmpc
   ];
 }
